646121ec2a4fe6e97e60931a938abdf6ff431cd2
Artur Hallmann <artur.hallmann@tui.de>
2021-07-28 18:08:47 +0200

    update to tauri 1.0 beta

--- frontend/src-tauri/src/main.rs
+++ frontend/src-tauri/src/main.rs
@@ -1,43 +1,10 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+all(not(debug_assertions), target_os = "windows"),
+windows_subsystem = "windows"
 )]
 
-use crate::plugins::instance_plugin::InstancePlugin;
-
-mod cmd;
-mod plugins;
-
 fn main() {
-  let instance_plugin = InstancePlugin::new();
-  tauri::AppBuilder::new()
-      .plugin(instance_plugin)
-    .invoke_handler(|_webview, arg| {
-      use cmd::Cmd::*;
-      match serde_json::from_str(arg) {
-        Err(e) => {
-          Err(e.to_string())
-        }
-        Ok(command) => {
-          match command {
-            Start { } => {
-              println!("Start");
-            },
-            Stop { } => {
-              println!("Stop");
-            },
-            LoadConfiguration { } => {
-              println!("LoadConfiguration");
-            },
-            #[allow(unused_variables)]
-            UpdateConfiguration { key, value } => {
-              println!("UpdateConfiguration");
-            }
-          }
-          Ok(())
-        }
-      }
-    })
-    .build()
-    .run();
+  tauri::Builder::default()
+      .run(tauri::generate_context!())
+      .expect("error while running tauri application");
 }

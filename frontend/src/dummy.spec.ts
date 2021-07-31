6680bb4cd998d004675c5b41ee0ea1b5292fa8c3
Artur Hallmann <artur.hallmann@tui.de>
2021-08-01 00:45:39 +0200

    fixed linting

--- frontend/src/dummy.spec.ts
+++ frontend/src/dummy.spec.ts
@@ -1,8 +1,5 @@
-// eslint-disable-next-line no-undef
 describe('dummy test', () => {
-    // eslint-disable-next-line no-undef
     it('should work', () => {
-    // eslint-disable-next-line no-undef
         expect(1 + 1).toBe(2)
     })
 })
--- frontend/src/pages/Dashboard.vue
+++ frontend/src/pages/Dashboard.vue
@@ -1,51 +1,50 @@
 <template>
-<div class="p-grid p-fluid dashboard">
-	<div class="p-col-12 p-lg-4">
-		<div class="card summary">
-			<span class="title">Instances</span>
-			<span class="detail">Number of configured instances</span>
-			<span class="count visitors" @click="sendTestMessage()">2</span>
-		</div>
-	</div>
-	<div class="p-col-12 p-lg-4">
-		<div class="card summary">
-			<span class="title">Sales</span>
-			<span class="detail">Number of purchases</span>
-			<span class="count purchases">534</span>
-		</div>
-	</div>
-	<div class="p-col-12 p-lg-4">
-		<div class="card summary">
-			<span class="title">Revenue</span>
-			<span class="detail">Income for today</span>
-			<span class="count revenue">$3,200</span>
-		</div>
-	</div>
-</div>
+  <div class="p-grid p-fluid dashboard">
+    <div class="p-col-12 p-lg-4">
+      <div class="card summary">
+        <span class="title">Instances</span>
+        <span class="detail">Number of configured instances</span>
+        <span class="count visitors" @click="sendTestMessage()">2</span>
+      </div>
+    </div>
+    <div class="p-col-12 p-lg-4">
+      <div class="card summary">
+        <span class="title">Sales</span>
+        <span class="detail">Number of purchases</span>
+        <span class="count purchases">534</span>
+      </div>
+    </div>
+    <div class="p-col-12 p-lg-4">
+      <div class="card summary">
+        <span class="title">Revenue</span>
+        <span class="detail">Income for today</span>
+        <span class="count revenue">$3,200</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import {defineComponent} from "vue";
-import { listen } from '@tauri-apps/api/event'
+import { defineComponent } from 'vue';
+import { listen } from '@tauri-apps/api/event';
 
-import { invoke } from '@tauri-apps/api/tauri'
+import { invoke } from '@tauri-apps/api/tauri';
 export default defineComponent({
-  components: {
-  },
+  components: {},
   setup() {
     return {
-      sendTestMessage: async() => {
+      sendTestMessage: async () => {
         console.log('listen to the_event');
-        await listen('the_event', event => {
-          console.log('event from rust', event)
+        await listen('the_event', (event) => {
+          console.log('event from rust', event);
           // event.event is the event name (useful if you want to use a single callback fn for multiple event types)
           // event.payload is the payload object
-        })
-        await invoke('my_custom_command')
+        });
+        await invoke('my_custom_command');
 
-        const config = await invoke('load_config')
-        console.log('config:', config)
-        await invoke('save_config')
+        const config = await invoke('load_config');
+        console.log('config:', config);
+        await invoke('save_config');
         /*
         // listen to the `click` event and get a function to remove the event listener
         // there's also a `once` function that subscribes to an event and automatically unsubscribes the listener on the first event
@@ -56,9 +55,8 @@ export default defineComponent({
           theMessage: 'Tauri is awesome!'
         })
          */
-
       }
-    }
+    };
   },
-})
+});
 </script>

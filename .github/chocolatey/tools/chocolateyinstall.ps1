70a1746f7291d98fb53f2b83ba9e4db8263910d9
Artur Hallmann <artur.hallmann@tui.de>
2021-08-07 03:03:04 +0200

    checksums for chocolatey releases

--- .github/chocolatey/tools/chocolateyinstall.ps1
+++ .github/chocolatey/tools/chocolateyinstall.ps1
@@ -20,7 +20,7 @@ $url        = 'https://github.com/arturh85/factorio-bot-tauri/releases/download/
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE_MSI_OR_MSU' #only one of these: exe, msi, msu
+  fileType      = 'MSI' #only one of these: exe, msi, msu
   url           = $url
   url64bit      = $url64
   #file         = $fileLocation
@@ -31,7 +31,7 @@ $packageArgs = @{
   # To determine checksums, you can get that from the original site if provided. 
   # You can also use checksum.exe (choco install checksum) and use it 
   # e.g. checksum -t sha256 -f path\to\file
-  checksum      = ''
+  checksum      = '__REPLACE_CHECKSUM__'
   checksumType  = 'sha256' #default is md5, can also be sha1, sha256 or sha512
   checksum64    = ''
   checksumType64= 'sha256' #default is checksumType
---
+++ frontend/update-checksum.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const crypto = require('crypto')
+const algorithm = 'sha256', shasum = crypto.createHash(algorithm)
+const cargoTomlPath = './src-tauri/Cargo.toml'
+const cargoToml = fs.readFileSync(cargoTomlPath, {encoding: 'utf8'})
+const matches = cargoToml.match(/version = "(.*?)"/)
+if (matches) {
+    const version = matches[1]
+    const filename = `src-tauri/target/release/bundle/msi/factorio-bot_${version}_x64.msi`, s = fs.ReadStream(filename)
+    s.on('data', function (data) {
+        shasum.update(data)
+    });
+    s.on('end', function () {
+        const checksum = shasum.digest('hex')
+        const filesToReplaceChecksum = [
+            '../.github/chocolatey/tools/chocolateyinstall.ps1'
+        ]
+        for (let filePath of filesToReplaceChecksum) {
+            let content = fs.readFileSync(filePath, {encoding: 'utf8'});
+            fs.writeFileSync(filePath, content.replaceAll('__REPLACE_CHECKSUM__', checksum))
+        }
+    })
+}
+

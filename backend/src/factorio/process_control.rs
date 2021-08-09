6e33e0c8a7c303f0f3fbfe1fad6fba5dc2773896
Artur Hallmann <artur.hallmann@tui.de>
2021-08-09 17:46:46 +0200

    cleanup

--- backend/src/factorio/process_control.rs
+++ backend/src/factorio/process_control.rs
@@ -135,6 +135,7 @@ pub async fn start_factorio(
         rcon.silent_print("").await.unwrap();
     }
 
+    // arrange factorio windows to fill the primary display
     #[cfg(windows)]
     {
         mod bindings {
@@ -148,33 +149,21 @@ pub async fn start_factorio(
             },
         };
         async_std::task::sleep(Duration::from_secs(client_count as u64)).await; // wait for window to be visible, hopefully
-
         static mut HWNDS: Vec<HWND> = Vec::new();
-        // let mut factorio_hwnds: Vec<HWND> = vec![];
         extern "system" fn enum_window(window: HWND, _: LPARAM) -> BOOL {
             unsafe {
-                // let foo: isize = store_hwnd.try_into().unwrap();
-                // let mut pntr = foo as *const StoreHWNDs;
-                // info!("DEBUG {:?}", store_hwnd.borrow());
-
                 let mut text: [u16; 512] = [0; 512];
                 let len = GetWindowTextW(window, PWSTR(text.as_mut_ptr()), text.len() as i32);
-
                 let text = String::from_utf16_lossy(&text[..len as usize]);
                 if !text.is_empty() && text.contains("Factorio ") && !text.contains("Factorio Bot")
                 {
                     HWNDS.push(window);
-                    info!("window {:?} {}", window, text);
                 }
-
                 BOOL(1)
             }
         }
-
         unsafe {
-            // let pntr = &mut store as *mut StoreHWNDs;
-            EnumWindows(Some(enum_window), LPARAM(0 as isize)).ok()?;
-            info!("result {:?}", HWNDS);
+            EnumWindows(Some(enum_window), LPARAM(0_isize)).ok()?;
             let max_width = GetSystemMetrics(SM_CXMAXIMIZED);
             let max_height = GetSystemMetrics(SM_CYMAXIMIZED);
             let count = HWNDS.len();
@@ -200,27 +189,23 @@ pub fn window_size(
     client_count: usize,
     client_index: usize,
 ) -> (i32, i32, i32, i32) {
-    if client_count == 1 {
-        (0, 0, width_full, height_full)
-    } else {
-        // cut into two columns and as many rows as needed
-        let cols = 2;
-        let col_index = (client_index % 2) as i32;
-        let rows = (client_count as f64 / 2.0).ceil() as i32;
-        let row_index = (client_index / 2) as i32;
-        let col_width = width_full / cols;
-        let row_height = height_full / rows;
-        (
-            col_width * col_index,
-            row_height * row_index,
-            if client_count % 2 == 1 && client_index == client_count - 1 {
-                width_full
-            } else {
-                col_width
-            },
-            row_height,
-        )
-    }
+    // cut into two columns and as many rows as needed
+    let cols = 2;
+    let col_index = (client_index % 2) as i32;
+    let rows = (client_count as f64 / 2.0).ceil() as i32;
+    let row_index = (client_index / 2) as i32;
+    let col_width = width_full / cols;
+    let row_height = height_full / rows;
+    (
+        col_width * col_index,
+        row_height * row_index,
+        if client_count % 2 == 1 && client_index == client_count - 1 {
+            width_full
+        } else {
+            col_width
+        },
+        row_height,
+    )
 }
 
 #[cfg(test)]
